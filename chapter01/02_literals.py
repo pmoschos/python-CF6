b2a51d6786bf198f520910c1d6d76737ae3f78b1
Panagiotis Moschos <pan.moschos86@gmail.com>
2024-11-11 20:22:14 +0200

    python-cf6

---
+++ chapter01/02_literals.py
@@ -0,0 +1,33 @@
+num1 = 10 #10
+num2 = 0b1010 #10
+num3 = 0x000A #10
+
+# strings
+str1 = 'CF'
+str2 = "CF"
+str3 = 'Cod\
+    ing'
+
+# multine strings
+str4 = '''Hello
+Coding
+Factory'''
+
+str5 = """Hello
+again!"""
+
+ioannis = "That's my name"
+
+ioannis = 'That is "nested string"'
+
+my_bool = True
+
+print(type(num1))
+print(type(num2))
+print(type(num3))
+print(type(str1))
+print(type(str2))
+print(type(str3))
+print(type(str4))
+print(type(str5))
+print(type(my_bool))
