ef9ece23b83c88a700859855cf089a4a9c45285e
Panagiotis Moschos <pan.moschos86@gmail.com>
2024-11-12 20:23:31 +0200

    python-cf6

---
+++ chapter01/21_str_slicing.py
@@ -0,0 +1,28 @@
+s = "Coding Factory"
+
+s1 = s[7]
+print(s1) # F
+
+s2 = s[:6]
+print(s2) # Coding
+
+s3 = s[7:] # Factory
+print(s3)
+
+s4 = s[:]
+print(s4)
+
+s5 = s[-1]
+print(s5)
+
+s6 = s[:-2] # Coding Facto
+print(s6)
+
+s7 = s[::] # Coding Factory
+print(s7)
+
+s8 = s[-1::-1] # Coding Factory (reversed)
+print(s8)
+
+s9 = s[::-1] # Coding Factory (reversed)
+print(s9)
