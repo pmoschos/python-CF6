96e2139821e68f0bb3b997962b0334e6adff761a
Panagiotis Moschos <pan.moschos86@gmail.com>
2024-11-12 19:28:42 +0200

    python-cf6

--- chapter01/14_short_circuit_app.py
+++ chapter01/14_short_circuit_app.py
@@ -1,1 +1,6 @@
-username = in
+username = input("Enter your username: ")
+email = input("Enter your email: ")
+
+valid_user = username or "User"
+
+print(f"Hello {valid_user}, " + ((email and f"your email is {email}") or ("please provide your email")))
