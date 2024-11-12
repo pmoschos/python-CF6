6963e9d3695aeb19a1db2911d08d26a0fb358e09
Panagiotis Moschos <pan.moschos86@gmail.com>
2024-11-12 18:38:41 +0200

    python-cf6

--- chapter01/06_input_demo.py
+++ chapter01/06_input_demo.py
@@ -4,3 +4,10 @@ is_stundent = input("Are you a student? (yes/no): ").lower() == "yes"
 
 print(f"name: {name}")
 print(f"is_student: {is_stundent}")
+
+if is_stundent:
+    print("You are a student")
+else:
+    print("You are not a student")
+
+
---
+++ chapter01/07_min_max_int.py
@@ -0,0 +1,8 @@
+import sys
+
+max_int = sys.maxsize
+
+print("Max int:", max_int)
+
+min_int = -sys.maxsize - 1
+print("Min int:", min_int)
