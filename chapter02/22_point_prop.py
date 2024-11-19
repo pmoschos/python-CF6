7143eebac766d556c55712ecd5b89a3cacf2ca12
Panagiotis Moschos <pan.moschos86@gmail.com>
2024-11-19 20:38:22 +0200

    python-cf6

--- chapter02/22_point_prop.py
+++ chapter02/22_point_prop.py
@@ -20,7 +20,8 @@ class Point:
         
     def __lt__(self, other):
         if isinstance(other, Point):
-            return self.__x < other.__x and self.__y < other.__y
+            return (self.__x <= other.__x and self.__y < other.__y) \
+                    or (self.__x < other.__x and self.__y <= other.__y)
         else:
             return False
     
@@ -48,20 +49,24 @@ class Point:
 
 
 def main():
-    p1 = Point(10, 20)
-    p2 = Point()
+    # p1 = Point(10, 20)
+    # p2 = Point()
 
-    p2.x = 10
-    p2.y = 22
+    # p2.x = 10
+    # p2.y = 22
 
-    print(p1)
-    print(p2)
-    print(p1 == p2)
+    # print(p1)
+    # print(p2)
+    # print(p1 == p2)
     # print(Point.get_count())
-    print(repr(p1))
+    # print(repr(p1))
+
+    # print(p1 < p2)
+
+    p1 = Point(11, 20)
+    p2 = Point(11, 20)
 
     print(p1 < p2)
-
 
 if __name__ == "__main__":
     main()
